9e47ee362e5233461a8a90e7f5fb6d9e6f9e5410
Blade83x2 <johannes_kraemer@gmx.de>
2023-03-27 18:26:44 +0200

    Version 3

--- git.sh
+++ git.sh
@@ -7,6 +7,15 @@ cd ..
 cd 12V
 sudo make
 cd ..
+cd h2o
+sudo make
+cd ..
+cd Shutdown
+sudo make
+cd ..
+cd Startup
+sudo make
+cd ..
 
 sudo git add -A && sudo git commit -m "Version 3"
 sudo git push -u origin main
