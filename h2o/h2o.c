cd0210b74f1a55cb7f41fa5545f56ef0a0e8d4bc
Blade83x2 <johannes_kraemer@gmx.de>
2023-04-17 07:00:17 +0200

    Parameter Übergabe + Hilfe + Prüfung eingebaut

--- h2o/h2o.c
+++ h2o/h2o.c
@@ -1,29 +1,159 @@
 #include <stdio.h>
-#include <unistd.h> // sleep()
+#include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+// Galonen pro Tag Filtermenge
+int gpd = 50;
 
-int gpd=50;   //  Filtermenge in GPD einstellen
-float literProMinute=0.07893; // bei 25gpd
-int pumpeRelaisNr=6; // Pumpe beim Aufruf deaktivieren
-float zielMengeInLiter=0.2;
+// Maximale Abwasser Literanzahl bei Reinigung
+int maxLiterAbwasserBeiReinigung = 5;
+
+// maxLiterAbwasserKanister (Überlaufschutz)
+int maxLiterAbwasserKanister = 10;
+
+// Aktuell gesameltes Abwasser (wird bei param1 = -r auf Null gesetzt)
+int aktuellesGesameltesAbwasser = 0;
+
+// Relaise Nr. für Pumpe & Boosterpumpe
+int pumpeRelaisNr = 6;  
+
+// Liter Pro Galone
+float literProGalone = 3.7854f;
+
+// Liter pro Tag (gpd * literProGalone)
+float literProTag = 0.f;
+
+// Relaise Nr. für Pumpe & Boosterpumpe
+//int pumpeRelaisNr = 6;
+
+// Maximale Abwasser Literanzahl bei Reinigung
+//int maxLiterAbwasserBeiReinigung = 5;
+
+// maxLiterAbwasserKanister (Überlaufschutz)
+//int maxLiterAbwasserKanister = 10;
+
+// Aktuell gesameltes Abwasser (wird bei param1 = -r auf Null gesetzt)
+//int aktuellesGesameltesAbwasser = 0;
+
+// Filtermenge (wird von param1 überschrieben)
+float filterMenge = 0.f;
+
+// Funktionen deklarieren
+void clearSystem();
+void showHelp();
+void showLogo();
+void abwasserZaehlerReset();
+
+// Programmstart
+int main(int argc, char* argv[]) { 
+    printf("\033[2J\033[1;1H"); // Bildschirm löschen
+    system("12V $pumpeRelaisNr 0"); // Wenn Pumpe an, dann ausschalten
+    showLogo();
+
+/*
+    printf("GPD: %d\n", gpd);
+    printf("Liter pro Galone %f\n", literProGalone);
+    printf("Liter pro Tag: %f \n\n\n", literProGalone*gpd);
+*/
 
 
-int runtime=0;  // laufzeit minuten vom abwasser
+    if(argc == 1) {
+        // Keine Parameter übergeben,Hilfe anzeigen
+        showHelp();
+        return 0;
+    }
+    // prüfen ob argv[1] -c oder -r oder -l enthält, wenn nicht, Hilfe anzeigen
+    const char * param1 = argv[1];
+    if ( (strcmp(param1, "-c") == 1) || (strcmp(param1, "-r") == 1) || (strcmp(param1, "-l") == 1)) {
+        showHelp();
+        return 0;
+    }
 
-void showHelp() {
-    printf("\n\e[0;31m Falsche Parameter! Beispiel:\n\n");
-    printf(" h2o -l x [Produziert x Liter Wasser]\n\n\e[0m");
+
+    // wenn ein parameter übergeben worden ist
+    if(argc == 2) {
+	// Wenn param1 gleich -c ist, Anlage spülen
+        if (strcmp(param1, "-c") == 0) {
+            clearSystem();
+        }
+        // Wenn param1 gleich -r ist, Literzähler für Abwasserabfang Kanister auf 0 setzen
+        else if (strcmp(param1, "-r") == 0) {
+            abwasserZaehlerReset();
+        }
+        else {
+	    showHelp();
+        }
+        return 0;
+    }
+    if(argc == 3) {
+        // Filtern, wenn param1 -l ist und param2 eine float Wert ist
+        const char * param1 = argv[1];
+        // Wenn param1 gleich -l ist
+        if (strcmp(param1, "-l") == 0) {
+            // und param2 größer als 0.1 ist
+            filterMenge = (float) atof(argv[2]);
+            if (filterMenge >= 0.1){
+                printf(" Wasser wird gefiltert:\n ");
+	        printf(" Filtermenge  = %f Liter\n", filterMenge);
+
+
+
+
+
+
+// TODO filterzeit ermitteln und pumpe für diese zeit anstellen
+
+
+
+
+
+
+
+            } else {
+                printf("Die minimale Wassermenge zum filtern muss mindestens 0.1 Liter betragen!\n\n");
+            }
+        }
+        else {
+            // Bei 2 Parametern kan param1 nur -l sein, wenn nicht, Hilfe anzeigen
+            showHelp();
+            return 0;
+        }
+    }
 }
 
+void clearSystem() {
+    printf("\nREINIGUNG\n\n");
+    // Hinweisen das Ventil gedreht werden muss
 
-int main(int argc, char**argv) { 
-    printf("\033[2J\033[1;1H"); // clear screen
-    system("12V $pumpeRelaisNr 0"); // disable relais
+// TODO zuerst eimer leer machen, dann bestätigen, dann reinigen
+
+
+}
+
+// Abwasser Kanister Literzähler auf 0 setzen
+void abwasserZaehlerReset() {
+    printf("\nAbwasser Reset\n\n");
+    // Abfragen ob Eimer gelert worden ist
+
+
+
+
+
+}
+
+// Zeigt die Hilfe an
+void showHelp() {
+    printf("\n\e[0;31m Falsche Parameter! Beispiel:\n\n");
+    printf(" h2o -l 0.2 [Produziert 0,2 Liter gefiltertes Wasser]\n");
+    printf(" h2o -c [Spült die Anlage durch (Zuerst Ventil öffnen)]\n");
+    printf(" h2o -r [Setzt Kanister für Abwasser auf Null]\n\n\e[0m");
+}
+
+void showLogo() {
     printf("\n\n\tHHHHHHHHH     HHHHHHHHH 222222222222222         OOOOOOOOO     \n");
     printf("\tH:::::::H     H:::::::H2:::::::::::::::22     OO:::::::::OO   \n"); 
     printf("\tH:::::::H     H:::::::H2::::::222222:::::2  OO:::::::::::::OO  \n");
@@ -44,31 +174,5 @@ int main(int argc, char**argv) {
     printf("\t/    / / \\/__ __/  __/  __\\  / ___\\  \\// ___/__ __/  __/ \\__/| \n");
     printf("\t|  __| | |  / \\ |  \\ |  \\/|  |     \\  /|    \\ / \\ |  \\ | |\\/|| \n");
     printf("\t| |  | | |_/| | |  /_|    /  \\___ |/ / \\___ | | | |  /_| |  || \n");
-    printf("\t\\_/  \\_\\____\\_/ \\____\\_/\\_\\  \\____/_/  \\____/ \\_/ \\____\\_/  \\| \n");
-
-
-    if(argc == 1) {
-        // direkt wasser filtern
-        printf("zeige tabelle mit eingabe für nummer\n");
-
-        return 0;
-    }
-
-
-
-
-
-
-
-    // wenn ein parameter übergeben worden ist
-    if(argc == 2) {
-        const char * liter=argv[1];
-	// direkt wasser filtern
-        printf("\n\n\tes werden %s liter produziert\n\n", liter);
-       
-
-        return 0;
-    }
-    //showHelp();
+    printf("\t\\_/  \\_\\____\\_/ \\____\\_/\\_\\  \\____/_/  \\____/ \\_/ \\____\\_/  \\| \n\n");
 }
-
