25b3d2036c5f3c5292dbe74e89d067dac7ef9514
Blade83x2 <johannes_kraemer@gmx.de>
2023-07-09 22:18:40 +0200

    h2o -clean Fortschrittsanzeige implementiert

--- h2o/h2o.c
+++ h2o/h2o.c
@@ -385,7 +385,7 @@ void clearSystem() {
 
 // Progress Bar
 void print_progress(size_t count, size_t max) {
-    const int bar_width = 54;
+    const int bar_width = 53;
     float progress = (float) count / max;
     int bar_length = progress * bar_width;
     printf("\r -> [");
