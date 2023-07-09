93405b81f37e9294e3e619369fb03333ac3f0da3
Blade83x2 <johannes_kraemer@gmx.de>
2023-07-09 11:25:33 +0200

    Konfiguration über Parameter hinzugefügt

--- 12V/12V.c
+++ 12V/12V.c
@@ -487,11 +487,11 @@ void getDataForConfigFile(int relais, void* config) {
         strpMax = readStdinLine();
         int maxRest;
         maxRest = pconfig->mcp.maxPConverter-pconfig->mcp.maxPMicroController;
-        if((int)strpMax > (int)maxRest){
-            printf("\n\e[0;31m Dieses Gerät hat einen zu hohen Watt Verbrauch. Es wird nie eingeschaltet werden können!\e[0m");
+        if((int)strpMax < (int)maxRest){
+            printf("\n\e[0;31m Dieses Gerät hat einen zu hohen Watt Verbrauch. Es wird nie eingeschaltet werden können!\e[0m\n");
         }
         if((int)strpMax < (int)1){
-            printf("\n\e[0;31m Dieses Gerät hat eine falsche Watt Angabe!\e[0m");
+            printf("\n\e[0;31m Dieses Gerät hat eine falsche Watt Angabe!\e[0m\n");
         }
         
 
