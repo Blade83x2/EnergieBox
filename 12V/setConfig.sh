5bc1c496c454ebbfbecf5e2532b788a4a8cc3315
Blade83x2 <johannes_kraemer@gmx.de>
2023-07-09 11:58:25 +0200

    Konfiguration über Parameter hinzugefügt

--- 12V/setConfig.sh
+++ 12V/setConfig.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 relais=$(echo $1);
 name=$(echo $2); 
-[ -z "$name" ] && name="N/A";
+
+VAR=$($name(;
+
+if [ -z "${VAR}" ]; then
+    echo "VAR is unset or set to the empty string"
+fi
+if [ -z "${VAR+set}" ]; then
+    echo "VAR is unset"
+fi
+if [ -z "${VAR-unset}" ]; then
+    echo "VAR is set to the empty string"
+fi
+if [ -n "${VAR}" ]; then
+    echo "VAR is set to a non-empty string"
+fi
+if [ -n "${VAR+set}" ]; then
+    echo "VAR is set, possibly to the empty string"
+fi
+if [ -n "${VAR-unset}" ]; then
+    echo "VAR is either unset or set to a non-empty string"
+fi
 
 
-
+sleep 3;
 
 activateOnStart=$(echo $3);
 pMax=$(echo $4);
