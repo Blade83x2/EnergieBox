cf3524789f3b822ffe9f88c3e7113e155a8e1c92
Blade83x2 <johannes_kraemer@gmx.de>
2023-07-09 13:19:56 +0200

    Konfiguration über Parameter hinzugefügt

--- 12V/12V.c
+++ 12V/12V.c
@@ -532,7 +532,7 @@ void getDataForConfigFile(int relais, void* config) {
 
     
     
-    printf ("sudo sh /Energiebox/12V/setConfig.sh $relais $strname $stractivateOnStart $strpMax");
+    echo "sudo sh /Energiebox/12V/setConfig.sh $relais $strname $stractivateOnStart $strpMax";
     
     
     
