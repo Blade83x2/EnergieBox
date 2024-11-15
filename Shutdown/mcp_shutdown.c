64a309efd136af5e4a2e2ce4266676b38b4268b4
“Blade83x2” <johannes_kraemer@gmx.de>
2024-11-15 07:01:27 +0100

    Update Startroutine

--- Shutdown/mcp_shutdown.c
+++ Shutdown/mcp_shutdown.c
@@ -24,12 +24,9 @@ int main(int argc, char**argv) {
     }
     mcp_initReg();
     //  Alle als OUTPUT definieren und ausschalten
-    mcp_writeRegister(0x00, 0x00);  // bank a auf output
-    mcp_writeRegister(0x01, 0x00);  // bank b auf output
-
     for(int i = 0; i<16; i++) {
-     //   mcp_pinMode(i, 0);
-     //   mcp_digitalWrite(i,1);
+        mcp_pinMode(i, 0);
+        mcp_digitalWrite(i,1);
         sprintf(command, "sh /Energiebox/12V/setIni.sh %d %d", (i+1), 0);
         system(command);
 	sleep(0.1);
@@ -37,21 +34,21 @@ int main(int argc, char**argv) {
 
 
     // 230 Volt
-        mcp_begin(7);
-        fd = wiringPiI2CSetup(MCP23017_ADDRESS | i2caddr);
-        if(fd <0) {
-            printf("wiringPiI2CSetup error!!!");
-            return -1;
-        }
-        mcp_initReg();
+    mcp_begin(7);
+    fd = wiringPiI2CSetup(MCP23017_ADDRESS | i2caddr);
+    if(fd <0) {
+        printf("wiringPiI2CSetup error!!!");
+        return -1;
+    }
+    mcp_initReg();
 
-        // Alle als OUTPUT definieren und ausschalten
-        for(int i = 0; i<16; i++) {
-            mcp_pinMode(i, 0);
-            mcp_digitalWrite(i,1);
-            sprintf(command, "sh /Energiebox/230V/setIni.sh %d %d", (i+1), 0);
-            system(command);
+    // Alle als OUTPUT definieren und ausschalten
+    for(int i = 0; i<16; i++) {
+        mcp_pinMode(i, 0);
+        mcp_digitalWrite(i,1);
+        sprintf(command, "sh /Energiebox/230V/setIni.sh %d %d", (i+1), 0);
+        system(command);
 	sleep(0.1);
-        }
+    }
     return 0;
 }
