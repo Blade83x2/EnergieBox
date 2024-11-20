4c9eb93bb89708cdffb7af3b98bba8684bab4a1c
“Blade83x2” <johannes_kraemer@gmx.de>
2024-11-20 22:04:32 +0100

    Grid implementiert

--- git.sh
+++ git.sh
@@ -4,30 +4,38 @@ if [ `id -u` -ne 0 ]
   then echo "Script kann nur mit root Rechten aufgerufen werden!"
   exit
 fi
+cd /Energiebox
 
 cd 230V
 sudo make
 cd ..
+
 cd 12V
 sudo make
 cd ..
+
 cd h2o
 sudo make
 cd ..
+
 cd Shutdown
 sudo make
 cd ..
+
 cd Startup
 sudo make
 cd ..
+
+cd Grid
+sudo make
+cd ..
+
 cd gui
 sudo gcc gui.cpp -o gui $(pkg-config --cflags gtk+-3.0 --libs)
 cd ..
+
 cd Tracer
 sudo gcc -Wall -o tracer tracer.c mymcp23017.o -lwiringPi -std=gnu99
-cd ..
-cd Grid
-sudo make
 cd ..
 
 
