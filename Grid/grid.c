b082452fc0c022248e087cdef30ba996f0edad45
“Blade83x2” <johannes_kraemer@gmx.de>
2025-06-19 19:57:25 +0200

    Neue Bilder & Beschreibungen in README.MD

--- Grid/grid.c
+++ Grid/grid.c
@@ -157,16 +157,15 @@ int main(int argc, char *argv[]){
                     return 1;
                 }
                 printf("\n\e[0;36m=== Ladeparameter ===\e[0m\n");
-                printf("  %-35s %6.2f V\n",  "Ladespannung (U):", supplyMaxVoltage);
-                printf("  %-35s %6.2f A\n",  "Ladestrom (A):", supplyMaxCurrent);
-                printf("  %-35s %6.2f W\n",  "Ladeleistung pro Stunde:", supplyLoadPower);
-                printf("  %-35s %6.2f W\n",  "Ladeleistung pro Sekunde:", supplyLoadPower / 3600);
+                printf("  %-35s %10.2f V\n",  "Ladespannung (U):", supplyMaxVoltage);
+                printf("  %-35s %10.2f A\n",  "Ladestrom (A):", supplyMaxCurrent);
+                printf("  %-35s %10.2f W\n",  "Ladeleistung pro Stunde:", supplyLoadPower);
+                printf("  %-35s %10.2f W\n",  "Ladeleistung pro Sekunde:", supplyLoadPower / 3600);
                 supplyLoadTimeSec = supplyLoadWattStunden / (supplyLoadPower / 3600);
-                printf("  %-35s %6.0f Sek\n", "Errechnete Ladezeit:", supplyLoadTimeSec);
-                printf("\e[0;36m========================\e[0m\n\n");
+                printf("  %-35s %10.0f Sek\n", "Errechnete Ladezeit:", supplyLoadTimeSec);
+                printf("\e[0;36m========================\e[0m\n\n");             
+                                                
                                 
-                                
-                
                 // prüfen ob bereits schon eine ladung am laufen ist
                 if (access("/Energiebox/Grid/isLoading.lock", F_OK) == 0) {
                     printf("\e[0;31m Es wird bereits aus dem Netz geladen!\n\n");
