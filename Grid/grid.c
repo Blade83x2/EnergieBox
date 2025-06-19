38725f0d07d38a225e8c3a34c5493c7aa3696925
“Blade83x2” <johannes_kraemer@gmx.de>
2025-06-19 20:30:49 +0200

    Neue Bilder & Beschreibungen in README.MD

--- Grid/grid.c
+++ Grid/grid.c
@@ -169,8 +169,8 @@ int main(int argc, char *argv[]){
                 }
                 printf("\n  %-26s %8.2f V\n",  "Ladespannung (U):", supplyMaxVoltage);
                 printf("  %-26s %8.2f A\n",  "Ladestrom (A):", supplyMaxCurrent);
-                printf("  %-26s %8.2f W\n",  "Ladeleistung pro Stunde:", supplyLoadPower);
                 printf("  %-26s %8.2f W\n",  "Ladeleistung pro Sekunde:", supplyLoadPower / 3600);
+                printf("  %-26s %8.2f W\n",  "Ladeleistung pro Stunde:", supplyLoadPower);
                 supplyLoadTimeSec = supplyLoadWattStunden / (supplyLoadPower / 3600);
                 printf("  %-26s %6.0f Sek\n", "Errechnete Ladezeit:", supplyLoadTimeSec);
                 char timeString[16];
