60e6325cdb72c811954637c634dffa2c6a9fe8b6
“Blade83x2” <johannes_kraemer@gmx.de>
2026-02-25 06:47:07 +0100

    Code Optimierung

--- build.sh
+++ build.sh
@@ -11,205 +11,76 @@
 #
 # sudo ./build.sh "Beschreibung der Änderung angeben!"
 #
-scriptPath=$(cd $(dirname "$0"); pwd);
-source "$scriptPath/System/spinner.sh" || exit 1;
-clear;
-echo -e "\n  _____                      _      ____            ";
-echo -e " | ____|_ __   ___ _ __ __ _(_) ___| __ )  _____  __";
-echo -e " |  _| | '_ \ / _ \ '__/ _\` | |/ _ \  _ \ / _ \ \/ /";
-echo -e " | |___| | | |  __/ | | (_| | |  __/ |_) | (_) >  < ";
-echo -e " |_____|_| |_|\___|_|  \__, |_|\___|____/ \___/_/\_\\";
-echo -e "                       |___/                        \n\n";
+scriptPath="$(cd "$(dirname "$0")" && pwd)"
+source "$scriptPath/System/spinner.sh" || exit 1
+clear
+cat << "EOF"
 
-echo -e " ***************************************************";
-echo -e " ***             BUILD PROJECT FILES             ***";
-echo -e " ***************************************************\n";
-if [ "$(id -u)" == "0" ]; then
-    echo -e "\n";
-else
-    echo "Script kann nur mit root Rechten aufgerufen werden!";
-    exit 1;
+  _____                      _      ____
+ | ____|_ __   ___ _ __ __ _(_) ___| __ )  _____  __
+ |  _| | '_ \ / _ \ '__/ _` | |/ _ \  _ \ / _ \ \/ /
+ | |___| | | |  __/ | | (_| | |  __/ |_) | (_) >  <
+ |_____|_| |_|\___|_|  \__, |_|\___|____/ \___/_/\_\
+                       |___/
+
+ ***************************************************
+ ***             BUILD PROJECT FILES             ***
+ ***************************************************
+
+EOF
+
+# Root prüfen
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Script kann nur mit root Rechten aufgerufen werden!"
+    exit 1
 fi
-sleep 1
-echo -e "Starte Compiler...\n"
-sleep 1
 
-start_spinner 'box@Energiebox:/Energiebox/230V:$ make'
-sleep 1
-cd /Energiebox/230V
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-stop_spinner $?
+echo -e "\nStarte Compiler...\n"
+BASE="/Energiebox"
+RESULT="$BASE/makeResult.txt"
 
+build() {
+    local dir="$1"
+    start_spinner "box@Energiebox:$dir:$ make"
+    cd "$BASE/$dir" || exit 1
+    sudo make clean  > /dev/null
+    sudo make format > /dev/null
+    sudo make        > "$RESULT"
+    local result=$?
+    if [[ $result -eq 2 ]]; then
+        stop_spinner 1
+        cat "$RESULT"
+        exit 1
+    fi
+    stop_spinner $result
+}
 
-start_spinner 'box@Energiebox:/Energiebox/12V:$ make'
-sleep 1
-cd /Energiebox/12V
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
+projects=(
+    "230V"
+    "12V"
+    "h3o2"
+    "Shutdown"
+    "Startup"
+    "Kolloid/sounds"
+    "Grid"
+    "gui"
+    "Status"
+    "Tracer"
+)
 
+for p in "${projects[@]}"; do
+    build "$p"
+done
 
-start_spinner 'box@Energiebox:/Energiebox/h3o2:$ make'
-sleep 1
-cd /Energiebox/h3o2
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
+rm -f "$RESULT"
 
-
-start_spinner 'box@Energiebox:/Energiebox/Shutdown:$ make'
-sleep 1
-cd /Energiebox/Shutdown
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
-
-
-start_spinner 'box@Energiebox:/Energiebox/Startup:$ make'
-sleep 1
-cd /Energiebox/Startup
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
-
-
-start_spinner 'box@Energiebox:/Energiebox/Kolloid/sounds:$ make'
-sleep 1
-cd /Energiebox/Kolloid/sounds
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-cd ..
-stop_spinner $?
-
-
-
-
-start_spinner 'box@Energiebox:/Energiebox/Grid:$ make'
-sleep 1
-cd /Energiebox/Grid
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
-
-
-start_spinner 'box@Energiebox:/Energiebox/gui:$ make'
-sleep 1;
-cd /Energiebox/gui
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
-
-
-start_spinner 'box@Energiebox:/Energiebox/Status:$ make'
-sleep 1;
-cd /Energiebox/Status
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
-
-
-start_spinner 'box@Energiebox:/Energiebox/Tracer:$ make'
-sleep 1;
-cd /Energiebox/Tracer
-sudo make clean  > /dev/null
-sudo make format  > /dev/null
-sudo make  > /Energiebox/makeResult.txt;
-result=$?;
-if [ "$result" -eq "2" ]; then
-    stop_spinner 1
-    cat /Energiebox/makeResult.txt;
-    exit 1;
-fi
-cd ..
-stop_spinner $?
-rm -f /Energiebox/makeResult.txt;
-
-
-if [[ -z $1 && -z $2 ]]; then
-    echo -e "\n\n";
-
-else
-    echo -e "\n\nUpdate Git Repository...";
+# Optional Git Update
+if [[ -n $1 || -n $2 ]]; then
+    echo -e "\n\nUpdate Git Repository..."
     git add -A
-    echo -e "\n";
     git commit -m "$1"
-    echo -e "\n";
     git push -u origin master
-    echo -e "\n";
 fi
 
+echo -e "\n\nFertig.\n"
 
